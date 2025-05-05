05aea9514b388554455ff2fdd06cb2bcca9a042d
Sharanji P <sharanji.p@fisdom.com>
2025-05-05 21:05:24 +0530

    gif crop

--- app/views/themes/default/treatments.php
+++ app/views/themes/default/treatments.php
@@ -162,7 +162,7 @@
                                     <div class="swiper-slide"> 
                                         <div class="box-shadow-extra-large services-box-style-01 hover-box last-paragraph-no-margin border-radius-4px overflow-hidden">
                                             <div class="position-relative box-image">
-                                            <img src="https://nalamdentalcare.in/assets/frontend/img/treatment/facial_trauma-VEED.gif" style="height: 220px;width: 100;">
+                                            <img src="https://nalamdentalcare.in/assets/frontend/img/treatment/facial_trauma-VEED.gif" style="height: 240px;width: 100;">
                                             </div>
                                             <div class="bg-white " style="height:190px;">
                                                 <div class="ps-50px pe-50px pt-35px sm-p-35px sm-pb-0">
